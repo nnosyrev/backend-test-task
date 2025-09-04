25e19e0b61708be5a9767aba521c3e26c58ef04f
Nick <nick@example.com>
2025-09-04 15:56:44 +0300

    Правка схемы данных

--- migrations/schema.init.sql
+++ migrations/schema.init.sql
@@ -1,14 +1,15 @@
-create table if not exists products
+CREATE TABLE IF NOT EXISTS products
 (
-    id int auto_increment primary key,
-    uuid  varchar(255) not null comment 'UUID товара',
-    category  varchar(255) not null comment 'Категория товара',
-    is_active tinyint default 1  not null comment 'Флаг активности',
-    name text default '' not null comment 'Тип услуги',
-    description text null comment 'Описание товара',
-    thumbnail  varchar(255) null comment 'Ссылка на картинку',
-    price float not null comment 'Цена'
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    uuid VARCHAR(36) NOT NULL COMMENT 'UUID товара',
+    category VARCHAR(255) NOT NULL COMMENT 'Категория товара',
+    is_active TINYINT DEFAULT 1 NOT NULL COMMENT 'Флаг активности',
+    name VARCHAR(255) NOT NULL COMMENT 'Название товара',
+    description TEXT COMMENT 'Описание товара',
+    thumbnail VARCHAR(255) COMMENT 'Ссылка на картинку',
+    price DECIMAL(10, 2) NOT NULL COMMENT 'Цена'
 )
-    comment 'Товары';
+DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+COMMENT 'Товары';
 
-create index is_active_idx on products (is_active);
+CREATE UNIQUE INDEX uuid_unique_key ON products (uuid);
